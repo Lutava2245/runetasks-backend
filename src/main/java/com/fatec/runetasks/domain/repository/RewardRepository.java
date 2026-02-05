514fa36eda67e6b5337e579b6d86dc2fbad29389
Luan T. Felix <lutavares.bros@gmail.com>
2026-02-04 21:23:38 -0300

    :sparkles: adiciona busca por id do usuário e status para recompensas

--- src/main/java/com/fatec/runetasks/domain/repository/RewardRepository.java
+++ src/main/java/com/fatec/runetasks/domain/repository/RewardRepository.java
@@ -6,11 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.fatec.runetasks.domain.model.Reward;
+import com.fatec.runetasks.domain.model.enums.RewardStatus;
 
 @Repository
 public interface RewardRepository extends JpaRepository<Reward, Long> {
     
     List<Reward> findByUserId(Long id);
+
+    List<Reward> findByUserIdAndStatus(Long id, RewardStatus status);
 
     List<Reward> findByUserIdAndPriceLessThanEqual(Long id, int price);
 
