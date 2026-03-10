c8b0eda405c394de6540e52580f838df0b18d757
Luan T. Felix <lutavares.bros@gmail.com>
2026-03-10 15:45:26 -0300

    :white_check_mark: configura banco de dados H2 para testes

--- src/test/java/com/fatec/runetasks/RunetasksApplicationTests.java
+++ src/test/java/com/fatec/runetasks/RunetasksApplicationTests.java
@@ -2,8 +2,10 @@ package com.fatec.runetasks;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RuneTasksApplicationTests {
 
 	@Test
