38c790927f774f60161b4e1d27f43a28456b8c25
Luan T. Felix <lutavares.bros@gmail.com>
2026-04-27 18:22:22 -0300

    :memo: adiciona documentação a configuração de email e seu utilitário

--- src/main/java/com/fatec/runetasks/config/EmailConfig.java
+++ src/main/java/com/fatec/runetasks/config/EmailConfig.java
@@ -8,15 +8,48 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
+/**
+ * Configuração de envio de emails.
+ * <p>
+ * Esta classe é responsável por configurar o serviço de envio de emails da
+ * aplicação, utilizando as propriedades definidas no arquivo de configuração
+ * para autenticação e conexão com o servidor SMTP. Ela cria um bean do tipo
+ * {@link JavaMailSender} que pode ser injetado em outras partes da aplicação
+ * para enviar emails de forma simples e eficiente.
+ * <p>
+ * 
+ * @author Luan T. Felix
+ */
 @Configuration
 public class EmailConfig {
 
+    /**
+     * O nome de usuário (endereço de email) usado para autenticação no servidor SMTP.
+     * <p>
+     * Ele é lido do arquivo de configuração da aplicação (application.properties) e
+     * deve ser um endereço de email válido que tenha permissão para enviar emails
+     * através do servidor SMTP configurado.
+     * <p>
+     */
     @Value("${spring.mail.username}")
     private String username;
 
+    /**
+     * A senha usada para autenticação no servidor SMTP.
+     * <p>
+     * Ela é lida do arquivo de configuração da aplicação (application.properties) e
+     * deve ser a senha correspondente ao endereço de email configurado como nome de usuário.
+     * <p>
+     */
     @Value("${spring.mail.password}")
     private String password;
 
+    /**
+     * Configura o JavaMailSender para a aplicação, definindo as propriedades de
+     * conexão com o servidor SMTP e autenticação.
+     * 
+     * @return um objeto {@link JavaMailSender} configurado para a aplicação
+     */
     @Bean
     JavaMailSender getJavaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
--- src/main/java/com/fatec/runetasks/util/EmailHelper.java
+++ src/main/java/com/fatec/runetasks/util/EmailHelper.java
@@ -10,6 +10,16 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Utilitário para enviar e-mails HTML usando o Thymeleaf como mecanismo de
+ * template.
+ * <p>
+ * Ele é responsável por processar templates Thymeleaf e enviar e-mails
+ * formatados como HTML.
+ * <p>
+ * 
+ * @author Luan T. Felix
+ */
 @Component
 @RequiredArgsConstructor
 public class EmailHelper {
@@ -18,6 +28,20 @@ public class EmailHelper {
 
     private final TemplateEngine templateEngine;
 
+    /**
+     * Envia um e-mail HTML para um destinatário específico usando um template
+     * Thymeleaf.
+     * <p>
+     * O método processa o template Thymeleaf com os dados fornecidos no contexto e
+     * envia o e-mail formatado como HTML para o destinatário especificado.
+     * <p>
+     * 
+     * @param to           endereço de e-mail do destinatário
+     * @param subject      assunto do e-mail
+     * @param templateName nome do template Thymeleaf
+     * @param context      contexto com os dados para o template
+     * @throws RuntimeException Caso ocorra um erro ao processar o e-mail HTML
+     */
     public void sendHtmlMessage(String to, String subject, String templateName, Context context) {
         try {
             String htmlContent = templateEngine.process(templateName, context);
@@ -35,5 +59,5 @@ public class EmailHelper {
             throw new RuntimeException("Erro ao processar e-mail HTML", e);
         }
     }
-    
+
 }
